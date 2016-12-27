ed952a3fd6b98381b83e2ac1d0f05f7387b09540
Aaron Turner <aaron@aaronthedev.com>
2016-12-26 23:07:10 -0800

    Added gifgen to our libs

--- .bash_libs/index.sh
+++ .bash_libs/index.sh
@@ -12,3 +12,4 @@ _source-all sexy_prompt
 # Write some aliases to wrap around imported libs
 alias json="sh /home/$(whoami)/.bash_libs/JSON.sh < "
 alias wifi="/home/$(whoami)/.bash_libs/wifi.sh "
+alias gifgen="/home/$(whoami)/.bash_libs/gifgen.sh "
