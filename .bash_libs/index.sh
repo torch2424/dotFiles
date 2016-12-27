0149a301045009ff2131704b7a806815601aa7ed
Aaron Turner <aaron@aaronthedev.com>
2016-12-26 23:41:21 -0800

    Fixed out improper function call

--- .bash_libs/index.sh
+++ .bash_libs/index.sh
@@ -2,7 +2,7 @@
 # This file is meant for sourcing all imported libraries
 
 #Source all function from .bashrc, load our theme
-_source-all .bash_libs/sexy_prompt
+_source_all .bash_libs/sexy_prompt
 
 # Write some aliases to wrap around imported libs
 alias json="sh /home/$(whoami)/.bash_libs/JSON.sh < "
