c9f18a97cd6e8452596cb50eab6a2259aa445c8a
Aaron Turner <aaron@aaronthedev.com>
2016-12-26 22:48:41 -0800

    Added JSON.sh to our dotfiles

---
+++ .bash_libs/JSON.sh/init_JSON.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# SImply add an alias for our sh file
+alias json="sh /home/$(whoami)/.bash_libs/JSON.sh/JSON.sh < "
--- .bash_libs/main.sh
+++ .bash_libs/main.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#Function to source from the user's .bash_libs
+_user-bash-libs-source() {
+	source /home/$(whoami)/.bash_libs/$1;
+}
+
 # Function to source all files in folder
 _source-all() {
 	 for f in /home/$(whoami)/.bash_libs/sexy_prompt/*; do source $f; done;
@@ -8,4 +13,4 @@ _source-all() {
 
 # This file is meant for sourcing all imported libraries
 _source-all sexy_prompt
-
+_user-bash-libs-source JSON.sh/init_JSON.sh
