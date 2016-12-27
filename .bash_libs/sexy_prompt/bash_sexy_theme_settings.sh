f5372c8edadad4bde20af048a57444629a9eba42
Aaron Turner <aaron@aaronthedev.com>
2016-12-26 22:49:22 -0800

    Added out theme settings

---
+++ .bash_libs/sexy_prompt/bash_sexy_theme_settings.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Set some of the colors mentioned by the sexy bash prompt
+# https://github.com/twolfson/sexy-bash-prompt#configurationi
+# Color Guide: https://linuxtidbits.files.wordpress.com/2008/08/tputcolors.png?w=776
+PROMPT_USER_COLOR="$(tput bold)$(tput setaf 9)"
+PROMPT_PREPOSITION_COLOR="$(tput bold)$(tput setaf 7)"
