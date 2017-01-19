560e499bd33222fca6e0997f6902b42095b14483
Aaron Turner <aaron@aaronthedev.com>
2017-01-18 22:41:20 -0800

    Made the functions index-ing alot more expandable

--- .bash_functions/index.sh
+++ .bash_functions/index.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 # source our bash function
-source ~/.bash_functions/curlsh.sh
-source ~/.bash_functions/showHiddenFiles.sh
-source ~/.bash_functions/hideHiddenFiles.sh
+source ~/.bash_functions/functions/*
