ad046fca4c722280dc3d3516a52101f2f36f6273
Aaron Turner <aaron@aaronthedev.com>
2017-04-04 16:29:57 -0700

    Added a script for install fail2ban easier

---
+++ .bash_install_scripts/installFail2Ban.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Find a line containing the text, and replace the entire line with the other passed text
+#Reference: http://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
+#@param $1 - The First argument, The Text to be found in a file
+#@param $2 - The second argument, the line the will be replacing the entire first line
+#@param $3 - The Third/Last argument, the file this function will be run against
+find-replace-line() {
+    sed -i "/$1/c$2" $3
+}
+
+# Install fail2ban
+sudo apt-get update
+sudo apt-get install fail2ban
+
+# Copy into a jail.local
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+# Replace some lines in the file
+find-replace-line "bantime =" "bantime = 10000000" /etc/fail2ban/jail.local
+find-replace-line "findtime =" "findtime = 6000" /etc/fail2ban/jail.local
+find-replace-line "maxretry =" "maxretry = 10" /etc/fail2ban/jail.local
+
+# Enable for SSH
+find-replace-line "[ssh]" "[ssh]\nenable=true" /etc/fail2ban/jail.local
+find-replace-line "[sshd]" "[sshd]\nenable=true" /etc/fail2ban/jail.local
+
+# Restart fail2ban
+sudo service fail2ban stop
+sudo service fail2ban start
+
+# Show the iptables to prove it is working
+sudo iptables -S
