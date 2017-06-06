e6247a6bd568ccc3a93f3cb82bbe5c6a4e540600
Aaron Turner <torchh@torchh-macbookpro.roam.corp.google.com>
2017-06-06 11:00:02 -0700

    Added a git fork syncing function
    The Github UI way was getting old and messing stuff up

---
+++ .bash_functions/functions/gitSyncForkToMaster.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+gitSyncForkToMaster() {
+
+    if [ "$#" -ne 1 ]; then
+        echo "Must pass fork git clone url"
+        echo "e.g  https://github.com/ampproject/amphtml.git"
+        echo "Exiting..."
+        return 1;
+    fi
+    # Add the remote, call it "upstream":
+
+    git remote add upstream $1
+
+    # Fetch all the branches of that remote into remote-tracking branches,
+    # such as upstream/master:
+
+    git fetch upstream
+
+    # Make sure that you're on your master branch:
+
+    git checkout master
+
+    # Rewrite your master branch so that any commits of yours that
+    # aren't already in upstream/master are replayed on top of that
+    # other branch:
+
+    git rebase upstream/master
+
+    # Force push to our master
+    git push -f origin master
+}
+
+# Alaises
+
+alias gitSyncFork='gitSyncForkToMaster'
+alias git-sync-fork='gitSyncFork'
+alias gitforksync='gitSyncFork'
+alias gitsyncfork='gitSyncFork'
