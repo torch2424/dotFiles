a51107e2f2f3369d002a30c03deb345cd7fc1b8d
Aaron Turner <aaron@aaronthedev.com>
2017-06-19 15:15:08 -0700

    Added a nice execute bash script from a line for better script testing

---
+++ .bash_functions/functions/executeBashFromLine.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+
+# Fucntion to execute a bash script starting at line number
+
+executefromline() {
+  if [ "$#" -ne 2 ]; then
+      echo "USAGE: executeFromLine.sh [script file] [line # to execute from]"
+  else
+      cat $1 | awk "NR>=$2" | bash
+  fi
+}
+
+alias bashfromline='executefromline'
+alias executebashfromline='executefromline'
+alias scriptfromline='executefromline'
+alias exectueatline='executefromline'
