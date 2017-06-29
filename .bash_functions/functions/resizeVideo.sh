bcbe0fb9a7b585ef6944b6cd3882f8f2b125f384
Aaron Turner <aaron@aaronthedev.com>
2017-06-29 11:49:05 -0700

    Made a short ffmpeg script

---
+++ .bash_functions/functions/resizeVideo.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+resizeVideo {
+
+if ["$#" -ne 2]; then
+    echo "shortcut for: https://trac.ffmpeg.org/wiki/Scaling%20(resizing)%20with%20ffmpeg"
+    echo "USAGE:"
+    echo "resizeVideo.sh [inputfile] [width to resize/scale to]"
+else
+    ffmpeg -i $1 -vf scale=$2:-1 "$2_$1"
+fi
+
+}
+
+alias scaleVideo="resizeVideo"
+alias sclaevideo="resizeVideo"
