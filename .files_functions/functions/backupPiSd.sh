36673c36889c42616f1b1b4f8434ef13fd1c6ebf
Aaron Turner <aaron@aaronthedev.com>
2017-07-31 19:53:33 -0700

    Updated bach script

--- .files_functions/functions/backupPiSd.sh
+++ .files_functions/functions/backupPiSd.sh
@@ -6,9 +6,10 @@ function backupPiSd() {
     else
         echo "This command will run dd. Please beware, and ensure this is the correct disk to be backed up."
         echo " "
-        
+        echo "Backing up pi sd card. This will take a while" 
+
         # From the pi hut: https://thepihut.com/blogs/raspberry-pi-tutorials/17789160-backing-up-and-restoring-your-raspberry-pis-sd-card 
-        sudo dd bs=4M if="$1" of="$2".img
+        sudo dd if="$1" of="$2".img
 
         # Mention piShrink
         echo "Image backup Completed!"
