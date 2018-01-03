b1d18ed1a5fc1a0959fbb8edc9bf928d70f35fa1
Aaron Turner <aaron@aaronthedev.com>
2018-01-03 00:15:03 -0800

    Removed the lets encrypt domains since I am a big caddy user now

--- .files_letsEncryptDomains.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Use --expand to add more domainss
-sudo letsencrypt certonly --standalone --expand -d aaronthedev.com -d files.aaronthedev.com -d mongo.aaronthedev.com -d blog.aaronthedev.com -d git.aaronthedev.com -d testbuild.aaronthedev.com -d leahrosegarza.com
