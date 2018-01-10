73053fbbc29ab63207cb6282434514bf978f2031
Aaron Turner <aaron@aaronthedev.com>
2018-01-09 16:02:46 -0800

    Added a nice skip ci alias for pedanco

--- .files_work/pedanco.sh
+++ .files_work/pedanco.sh
@@ -18,18 +18,23 @@ alias woprengine='WORKERS=ConversationEventWorker rake sneakers:run'
 # Skip CI for pedanco
 # https://gist.github.com/shytikov/3132078
 function pedanco_gc() {
-	
-	if [ "$#" -eq 1 ]; then
-	    echo "$1 --skip-ci" > "$1"
-	else
-		echo " "
-		echo "pedanco_gc USAGE:"
-		echo " "
-		echo "pedanco_gc 'COMMIT_MESSAGE_IN_QUOTES'"
-		echo " "
-	    echo "Aborting commit due to empty commit message."
+
+	if [[ $(pwd) == *"pedanco"* ]] || [[ $(pwd) == *"wopr"* ]]; then
+
+    if [ "$#" -eq 1 ]; then
+  	    git commit -m "$1 --skip-ci"
+  	else
+  		echo " "
+  		echo "pedanco_gc USAGE:"
+  		echo " "
+  		echo "pedanco_gc 'COMMIT_MESSAGE_IN_QUOTES'"
+  		echo " "
+  	    echo "Aborting commit due to empty commit message."
+  	fi
+
+  else
+    git commit
 	fi
-	
 }
 
 alias gc='pedanco_gc'
