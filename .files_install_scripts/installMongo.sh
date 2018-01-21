d98e1381b0cbb9ef693f4725f4cf70cd68340f6c
Aaron Turner <aaron@aaronthedev.com>
2018-01-20 21:53:35 -0800

    Improved systemctl aliases

--- .files_install_scripts/installMongo.sh
+++ .files_install_scripts/installMongo.sh
@@ -3,8 +3,8 @@
 # From: https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-16-04
 
 echo " "
-echo "Install MongoDB..."
-echo "This will require some confirmation in some places, so be ready to press enter, and say yes :)"
+echo "Installing MongoDB..."
+echo "This will require sudo..."
 echo " "
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
@@ -18,10 +18,10 @@ echo " "
 echo "MongoDB Installed, and enabled at boot, showing systemctl status..."
 echo " "
 
-sudo systemctl status mongod
+sudo systemctl status --no-pager mongod
 
 echo " "
-echo "If you see a status here, Mongo is installed! And control mongo with systemctl!"
+echo "If you see a status here, Mongo is installed! And control mongo (mongod) with systemctl!"
 echo " "
 echo "Done! Enjoy Losing your data! Jk! See the joke below:"
 echo "https://www.reddit.com/r/mongodb/comments/43m2om/how_exactly_does_mongodb_lose_data_and_is_it/"
