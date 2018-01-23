950645d31f8ee882d689dbaf50e0b8d55293cc75
Aaron Turner <aaron@aaronthedev.com>
2018-01-22 23:55:28 -0800

    Updated hyper, and started testing failover

--- .hyper.js
+++ .hyper.js
@@ -70,7 +70,7 @@ module.exports = {
     copyOnSelect: false,
 
     // URL to custom bell
-    bellSoundURL: 'https://files.aaronthedev.com/$/s4rq9'
+    bellSoundURL: 'https://files.aaronthedev.com/$/nsspe'
 
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
