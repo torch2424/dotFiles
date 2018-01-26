49a9f497149827b59f1fe3e4cf3b65aae29e4948
Aaron Turner <aaron@aaronthedev.com>
2018-01-26 01:17:14 -0800

    Cleaned up youtube to mp3

--- .files_functions/youtubemp3.sh
+++ .files_functions/youtubemp3.sh
@@ -3,13 +3,15 @@
 # Function to download a youtube video and convert to mp3
 __youtubemp3() {
 	if [ $# -ne "2" ]; then
-		echo "Youtube mp3. First argument url. Second argument, output path"
+		echo "Youtube Video to mp3"
+		echo "Usage:"
+		echo "./youtubemp3 [Youtube URL] [Output Path e.g './video.mp3']"
 		return
 	fi
 
 	if hash ytdl 2>/dev/null; then
         	ytdl $1 > /tmp/$2
-		ffmpeg -i /tmp/$2 -acodec libmp3lame -aq 4 $2
+			ffmpeg -i /tmp/$2 -acodec libmp3lame -aq 4 $2
 	else
         	echo "ytdl is not installed. run: npm install -g ytdl"
 	fi
@@ -19,3 +21,4 @@ alias youtubemp3='__youtubemp3'
 alias youtube-mp3="youtubemp3"
 alias youtubeMp3="youtubemp3"
 alias youtubetomp3="youtubemp3"
+alias youtube2mp3="youtubemp3"
