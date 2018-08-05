7c046ef76049d9fe20c98495351ba047c35f369a
Aaron Turner <aaron@aaronthedev.com>
2018-08-04 23:14:36 -0700

    Renamed .filesrandom and added a google dynamic dns script

---
+++ .files_scripts/googleDomainsDynamicDns.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Modified From: https://gist.github.com/cyrusboadway/5a7b715665f33c237996
+
+# Add this to your crontab for autmated usage
+# https://www.raspberrypi.org/documentation/linux/usage/cron.md
+# Runs every day at midnight
+# 0 0 * * * ./googleDomainsDynamicDns.sh
+
+# Make sure we pass our version
+if [ "$#" -ne 3 ]; then
+
+  echo "USAGE:"
+  echo "./googleDomainsDynamicDns.sh [USERNAME] [PASSWORD] [HOSTNAME]"
+
+else
+
+  # Update Google DNS Record
+  URL="https://$1:$2@domains.google.com/nic/update?hostname=$3"
+
+  echo " "
+  echo " "
+
+  echo "Making a Request to: $URL"
+
+  echo "See response values here: https://support.google.com/domains/answer/6147083"
+
+  echo " "
+  echo " "
+
+  curl -s $URL
+
+  echo " "
+  echo " "
+
+  echo "Done!"
+
+fi
