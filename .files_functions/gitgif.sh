573d1b7c469543210008cf4437c5155347620d5c
Aaron Turner <aaron@aaronthedev.com>
2018-09-14 11:12:27 -0700

    Added start time and end time to gitgif

--- .files_functions/gitgif.sh
+++ .files_functions/gitgif.sh
@@ -2,14 +2,14 @@
 
 
 __gitgif() {
-  if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  if [ $# -lt 1 ] || [ $# -gt 4 ]; then
     echo "This will make a best guess to convert a video into a github friendly .gif"
     echo "Works best on videos ~30s"
     echo " "
     echo " "
     echo "USAGE:"
     echo " "
-    echo "    gitgif [video file e.g (example.mp4)] [Optional: width of new video to scale to e.g 320]"
+    echo "    gitgif [video file e.g (example.mp4)] [Optional: Where to start the gif in the input video. In seconds. Default: 0] [Optional: Where to end the gif in the input video. In Seconds. Default: Video End time ] [Optional: width of new video to scale to e.g 320. Default: 500]"
   elif ! type "ffmpeg" > /dev/null 2>&1; then
     echo "ffmpeg and gifgen are required for $0"
     echo "OSX: brew install lukechilds/tap/gifgen"
@@ -31,19 +31,40 @@ __gitgif() {
     # Define our temporary gif path
     TEMP="/tmp/gitgif.gif"
 
+    # Set a start time
+    STARTTIME=0
+    if [ -z $2 ]; then
+      unset STARTTIME
+    else
+      STARTTIME=$2
+    fi
+
+    # Set a durtation
+    ENDTIME=0
+    if [ -z $3 ]; then
+      unset ENDTIME
+    else
+      ENDTIME=$3
+    fi
+
+
     # Resize the video to about 500
     BASEWIDTH=500
     # Check if we passed a width
-    if [ -z $2 ]; then
+    if [ -z $4 ]; then
       BASEWIDTH=500
     else
-      BASEWIDTH=$2
+      BASEWIDTH=$4
     fi
 
     if [ $WIDTH -gt $BASEWIDTH ]; then
       # Allow CTRL C
       trap "exit" INT
-      until ffmpeg -y -i $1 -r 30 -vf scale=$BASEWIDTH:-1 $TEMP
+      until ffmpeg -y -i $1 -r 30 \
+        ${STARTTIME:+ -ss "${STARTTIME}"} \
+        ${ENDTIME:+ -to "${ENDTIME}"} \
+        -vf scale=$BASEWIDTH:-1 \
+        $TEMP
       do
         # Allow CTRL C
         sl -e
