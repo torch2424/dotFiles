1501e2fc3ba8680a1e7b7503b52d0b28a3a15cf5
Aaron Turner <aaron@aaronthedev.com>
2019-01-29 10:09:36 +0000

    Cleaned up the goaccess generate, and updated caddy log rotating

--- .files_caddy/aaronTheDev-proxy/goaccess/generateGoaccessHtml.sh
+++ .files_caddy/aaronTheDev-proxy/goaccess/generateGoaccessHtml.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 while [ true ]
 do
-  goaccess ../access.log --log-format=VCOMBINED -e 127.0.0.1 -e 0.0.0.0 -o index.html
-  sleep 60
+  # Staandard ouput
+  echo " "
+  echo "Generating index.html"
+  echo " "
+  cat ../access* | goaccess --log-format=VCOMBINED -e 127.0.0.1 -e 0.0.0.0 -o index.html
+  
+  # JSON output
+  echo " "
+  echo "Generating index.json"
+  echo " "
+  cat ../access* | goaccess --log-format=VCOMBINED -e 127.0.0.1 -e 0.0.0.0 -o index.json
+  
+  # No Crawlers Output
+  echo " "
+  echo "Generating ignorecrawlers.html"
+  echo " "
+  cat ../access* | goaccess --log-format=VCOMBINED -e 127.0.0.1 -e 0.0.0.0 --ignore-crawlers -o ignorecrawlers.html
+
+  # Sleep for a while
+  echo " "
+  echo "Sleeping..."
+  echo " "
+  sleep 2h
 done
