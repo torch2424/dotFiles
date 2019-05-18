9eb80f9adc9064ecc1efee6ea2473b9f5fec8f8c
Aaron Turner <aaron@aaronthedev.com>
2019-05-18 10:55:01 +0000

    Added port number to rsync script

--- .files_caddy/aaronTheDev-proxy/goaccess/rsyncLogs.sh
+++ .files_caddy/aaronTheDev-proxy/goaccess/rsyncLogs.sh
@@ -11,7 +11,7 @@ do
 
   # rsync the logs to the main proxy server
   # https://www.tecmint.com/ssh-passwordless-login-using-ssh-keygen-in-5-easy-steps/
-  rsync -avz -e ssh ../logs/access* torch2424@proxy.aaronthedev.com:/home/torch2424/.files_caddy/aaronTheDev-proxy/logs
+  rsync -avz -e 'ssh -p 1124' ../logs/access* torch2424@proxy.aaronthedev.com:/home/torch2424/.files_caddy/aaronTheDev-proxy/logs
 
 
   # Sleep for a while
