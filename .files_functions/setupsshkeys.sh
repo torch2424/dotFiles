2f894a6241682a8e437ec6bd93c95d206a0ef587
Aaron Turner <aaron@aaronthedev.com>
2019-05-18 08:27:14 +0000

    Added some better logging

--- .files_functions/setupsshkeys.sh
+++ .files_functions/setupsshkeys.sh
@@ -8,11 +8,27 @@ setupSshKeys() {
     echo "setupSshKeys USAGE:"
     echo "setupSshKeys [username] [url]"
   else
-
+    
+    echo " "
     echo "Will have to login for $1@$2 multiple times :)"
+    echo " "
 
-    echo "Generating passwordless keys..."
-    ssh-keygen -f /home/$(whoami)/.ssh/id_rsa -t rsa -N ''
+    echo "--- LOCAL SERVER ---"
+    echo " "
+
+    echo "Generating passwordless keys if none..."
+    SSH_FILE=/home/$(whoami)/.ssh/id_rsa
+    if test -f "$SSH_FILE"; then
+      echo "$SSH_FILE was found, skipping generation..."
+    else
+      echo "$SSH_FILE was NOT found, generating..."
+      ssh-keygen -f $SSH_FILE -t rsa -N ''
+    fi
+
+    echo " "
+    echo "--- REMOTE SERVER ---"
+    echo " "
+
 
     echo "Creating .ssh directory if none"
     ssh $1@$2 mkdir -p .ssh
@@ -23,9 +39,11 @@ setupSshKeys() {
     echo "Setting .ssh permissions..."
     ssh $1@$2 "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 
+    echo " "
     echo "Done!"
     echo "Please try to SSh now:"
     echo "$1@$2"
+    echo " "
   fi
 }
 
