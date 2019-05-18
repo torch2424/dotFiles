f46b646eb5ce203d5c3e3173ca4022fa4bd75fea
Aaron Turner <aaron@aaronthedev.com>
2019-05-18 07:28:21 +0000

    Started some services for rsyncing logs

---
+++ .files_caddy/aaronTheDev-proxy/goaccess/rsyncLogs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+while [ true ]
+do
+  
+  # Some intro
+  echo " "
+  echo "Running goaccess logs..."
+  echo "Current Directory:"
+  pwd
+  echo " "
+
+  # rsync the logs to the main proxy server
+
+
+  # Sleep for a while
+  echo " "
+  echo "Sleeping..."
+  echo " "
+  sleep 2h
+done
--- .files_functions/installSystemctlService.sh
+++ .files_functions/installSystemctlService.sh
@@ -2,7 +2,7 @@
 
 installSystemctlService() {
   echo "Copying to /etc/systemd/system"
-  cp $1 /etc/systemd/system
+  sudo cp $1 /etc/systemd/system
 }
 
 alias install-systemctl-service='installSystemctlService'
