dbb55672823bf7eb0af25d97ca6007c26c9bd04d
Aaron Turner <aaron@aaronthedev.com>
2019-08-26 11:09:32 -0700

    Added a wasm cmp function

---
+++ .files_functions/cmpwasm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Function to compare wasm binaries
+# https://superuser.com/questions/125376/how-do-i-compare-binary-files-in-linux
+
+__cmpWasm() {
+  cmp -l $1 $2 | gawk '{printf "%08X %02X %02X\n", $1, strtonum(0$2), strtonum(0$3)}'
+}
+
+# Aliases for this function
+alias cmpwasm='__cmpWasm'
+alias cmp-wasm="cmpwasm"
