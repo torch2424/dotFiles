59b03e16f960bb36f8ab11f8e9b9950cbaa4ae3f
Julian Poyourow <julianpoyo@gmail.com>
2020-02-14 21:28:32 -0800

    Add pynvim

--- installNeoVim.sh
+++ installNeoVim.sh
@@ -8,11 +8,16 @@ chmod u+x nvim.appimage
 
 mkdir -p ~/.config/nvim
 
+echo "Setting nvim to use standard vim .vimrc"
+
 cat <<EOT >> ~/.config/nvim/init.vim
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 EOT
+
+echo "Installing pynvim extensions for cool addon support!"
+pip3 install --user pynvim
 
 echo "Now alias nvim and/or vim to nvim.appimage"
 
